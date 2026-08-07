ac98c24842a538f1983f3c64c55de5bdfc51b62b
nathan challande <nathanchallande@nathans-macbook-air.home>
2026-08-07 21:39:03 +0200

    start of the notebook

--- src/load_data.py
+++ src/load_data.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 import pandas as pd
 
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
 def load_data(file_path: str):
     """
     Load all beijing air quality csv files into a single data frame
     """
-    file_path = Path(file_path)
-    csv_files = sorted(file_path.glob("*.csv"))
+    file_path = PROJECT_ROOT / file_path
+    csv_files = sorted(file_path.glob("*.csv")) # *.csv means "finds every csv file"
     dfs = []
-
+    if not csv_files:
+        raise FileNotFoundError(f"No CSV files found in {file_path}")
     for file in csv_files:
         df = pd.read_csv(file)
         dfs.append(df)
